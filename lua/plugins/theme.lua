6b30e4b2f999baea6f1d035359b9cf008515c752
Ethan <ethanbutformal@gmail.com>
2023-08-08 10:26:39 -0700

    Initial Commit

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+      -- colorscheme = "catppuccin-latte",
+    },
+  },
+}
