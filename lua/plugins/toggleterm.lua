d260ddf1997f0f300b87ab6e9737b8607c86a5c8
Ethan <ethanbutformal@gmail.com>
2023-10-14 02:39:49 -0700

    Removed the toggleterm plugin

--- lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    “akinsho/toggleterm.nvim”,
-    version = “*”,
-    keys = {
-      { “<leader>td”, “<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>”, desc = “Open a horizontal terminal at the Desktop directory” }
-    },
-  },
-}
